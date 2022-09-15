ca1e1b8b4247d80cfb5064398ec929760560807e
Andrew <70990567+apakula036@users.noreply.github.com>
2022-09-14 20:04:13 -0700

    Initial commit

---
+++ pythonProjectOne.py
@@ -0,0 +1,13 @@
+print("Poop all over")
+
+playing = input("Do you like to eat poo? ")
+
+if playing != "yes":
+    quit()
+print("okay lets poop")
+
+answer = input("what des CPU stand for? ")
+if answer == "central processing unit":
+    print("Correct!")
+else: 
+    print("Incorrect!")
