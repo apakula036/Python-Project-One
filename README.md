71cf08e5ceab21da3ea945fd1894265e632c01d9
Andrew <70990567+apakula036@users.noreply.github.com>
2022-09-18 09:49:59 -0700

    Finished first project
    Added the questions and finished the project. Takes an input and gives an output. Does the math for the score.

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # Python-Quiz-Game
  
+First python project. Basic quiz game with some questions that outputs a percent at the end. Takes an input and does some math for score. 
--- pythonProjectOne.py
+++ pythonProjectOne.py
@@ -1,13 +1,42 @@
-print("Poop all over")
+print("Hello and welcome to the game!")
 
-playing = input("Do you like to eat poo? ")
+answer = input("Do you want to play a tech quiz? ")
 
-if playing != "yes":
+if answer.lower() != "yes":
     quit()
 print("okay lets poop")
+score = 0
 
 answer = input("what des CPU stand for? ")
-if answer == "central processing unit":
+if answer.lower() == "central processing unit":
     print("Correct!")
+    score += 1
+    print("Current score: " + str(score))
 else: 
     print("Incorrect!")
+
+answer = input("what des GPU stand for? ")
+if answer.lower() == "graphics processing unit":
+    print("Correct!")
+    score += 1
+    print("Current score: " + str(score))
+else: 
+    print("Incorrect!")
+
+answer = input("what des RAM stand for? ")
+if answer.lower() == "random access memory":
+    print("Correct!")
+    score += 1
+    print("Current score: " + str(score))
+else: 
+    print("Incorrect!")
+    
+answer = input("what des PSU stand for? ")
+if answer.lower() == "power supply unit":
+    print("Correct!")
+    score += 1
+    print("Current score: " + str(score))
+else: 
+    print("Incorrect!")
+
+print("You got " + str((score / 4) * 100) +"%")
